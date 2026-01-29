9668ccce1350f4a3248674f4e28aa97446feb59f
BekzodCS <bekzodabduraxmanov02@gmail.com>
2026-01-29 19:28:04 +0500

    Add basic backend security middleware

--- chat_app/server/index.js
+++ chat_app/server/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
 
+// Security & parsing
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// Health check route
 app.get("/", (req, res) => {
-    res.send("Secure Chat Backend is running");
+    res.json({ status: "Secure Chat Backend running" });
 });
 
 app.listen(3001, () => {
